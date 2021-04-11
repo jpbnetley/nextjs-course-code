865fce66678e6fe255af93a4e86e0629795c7013
Jonathan Netley <jpbnetley@gmail.com>
2021-04-11 22:59:19 +0200

    removed old imports

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
-
 function HomePage(props) {
   return (
     <Fragment>

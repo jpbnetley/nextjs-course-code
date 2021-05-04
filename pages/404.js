0ddd2364171b5d0139c45881521cecda2f144e9f
Jonathan Netley <jpbnetley@gmail.com>
2021-05-04 22:47:56 +0200

    completed pages and file based routing

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default  function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>
+        Page not found
+      </h1>
+    </div>
+  )
+}
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,11 +4,21 @@ export default function ClientsPage() {
   const router = useRouter()
   console.log(router.query);
 
+  const loadProjectHandle = () => {
+    //load data...
+    router.push('/clients/max/project-a')
+    //router.replace('/clients/max/project-a')
+    
+    // router.push({pathname: '/clients/[id]/[clientProjectId]',
+    // query: {id: 'max', clientProjectId: 'project-a'}})
+  }
+
   return (
     <div>
       <hl>
         The projects of a Given Client
       </hl>
+      <button onClick={loadProjectHandle}>Load project A</button>
     </div>
   )
 }

1bdbb8f4f161f52c3ca845c262f7fb15c9f9b8a1
Jonathan Netley <jpbnetley@gmail.com>
2021-11-14 12:44:36 +0200

    feat: added data to catch-all page (81)

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,6 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,10 +1,33 @@
-import router from 'next/router'
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
 
 const FilterEventsPage = () => {
+  const router = useRouter()
+  const filterData = router.query.slug
+
+  if (!filterData) return (<p className='center'>Loading...</p>)
+
+  const [year, month] = filterData
+  //+numYear: the + converts it to a number
+  const numYear = +year
+  const numMonth = +month
+
+  const isValidMonthRange = numMonth < 1 || numMonth > 12
+  const isValidYearRange = numYear > 2030 || numYear < 2021
+
+  if (isNaN(numYear) || isNaN(numMonth) || isValidYearRange || isValidMonthRange) return (<p>Invalid filter, please adjust your values</p>)
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
+
+  const filteredEventsLength = filteredEvents?.length || 0
+
+  if (filteredEventsLength === 0) return (<p>No events found for the chosen filter!</p>)
+
   return (
-    <div>
-      <h1>Filter events</h1>
-    </div>
+    <>
+      <EventList items={filteredEvents} />
+    </>
   )
 }
 

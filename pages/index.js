9ab4c699403af99c47bbd209a221aefc96bb80e3
Jonathan Netley <jpbnetley@gmail.com>
2021-11-18 21:22:16 +0200

    get static props additional (95)

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,19 @@ export const getStaticProps = async () => {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/not-found'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
+
   return {
     props: {
       products: data.products

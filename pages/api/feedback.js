fafa62c99b9183bcc6546e66dc35c6e2d6b5e2fc
Jonathan Netley <jpbnetley@gmail.com>
2022-03-06 09:34:06 +0200

    feat: added api post route

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+
 const handler = (req, res) => {
   //server side code
-  res.status(200).json({ message: 'This works!' })
+  if (req.method === 'POST') {
+    const { email, text } = req.body
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback: text
+    }
+
+    //store in db / file
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData)
+    data.push(newFeedback)
+    fs.writeFileSync(filePath, JSON.stringify(data))
+    res.status(201).json({ message: 'Success!', feedback: newFeedback })
+  } else {
+    res.status(200).json({ message: 'This works!' })
+  }
 }
 
 export default handler
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
+import { useRef } from 'react'
+
 function HomePage() {
+  const emailInputRef = useRef()
+  const feedbackInputRef = useRef()
+
+  const submitFormHandler = event => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value
+    const enteredFeedback = feedbackInputRef.current.value
+
+   
+
+    fetch()
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='mail' id='email' ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your feedback</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
